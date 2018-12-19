ecd05f1004d557f6894cf4943c134e79644bf385
Peter Xie <515971172@qq.com>
2018-12-19 15:07:13 +0800

    lastest

---
+++ Assets/Scripts/SceneLogic/Elevator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    private int speed = 30;
+
+    //高点
+    private int startPoint = 80;
+
+    //低点
+    private int endPoint = -80;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0,-speed);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(this.gameObject.GetComponent<RectTransform>().localPosition);
+        if(this.gameObject.GetComponent<RectTransform>().localPosition.y >= startPoint)
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        if(this.gameObject.GetComponent<RectTransform>().localPosition.y <= endPoint)
+            this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+
+    }
+}
---
+++ Assets/Scripts/Trigger/HotLadderTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotLadderTrigger : MonoBehaviour
+{
+    public GameObject fire;
+
+    public GameObject ladder;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fire.active == false)
+        {
+            ladder.SetActive(true);
+        }
+        
+    }
+}
