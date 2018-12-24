1d980b66d118f665e2e9560ab283918123178aa0
Peter Xie <515971172@qq.com>
2018-12-24 21:14:14 +0800

    lastest

---
+++ Assets/Scripts/Trigger/DoorControlTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControlTrigger : MonoBehaviour
+{
+    // 需要激活的物体
+    public GameObject obj;
+
+    private bool enable = false;
+
+    public void Update()
+    {
+        if (enable)
+        {
+            if (GamePersist.GetInstance().hero.interEnable)
+            {
+                this.obj.SetActive(true);
+            }
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        this.enable = true;
+
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        this.enable = true;
+    }
+
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        this.enable = false;
+    }
+}
--- Assets/Scripts/Trigger/DoorTrigger.cs
+++ Assets/Scripts/Trigger/DoorTrigger.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class DoorTrigger : MonoBehaviour {
 
 
-    // 把人物移动到上一层
+    public int toX;
+
+    public int toY;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<Hero>() != null)
         {
             Debug.Log("移动向上一层");
-            GamePersist.GetInstance().hero.HeroMove(new Vector2(-220, 210));
+            GamePersist.GetInstance().hero.HeroMove(new Vector2(toX, toY));
             //GamePersist.GetInstance().currentLevel++;
         }
     }
