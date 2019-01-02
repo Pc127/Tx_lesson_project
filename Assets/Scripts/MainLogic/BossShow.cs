374c9da85f3141eee8fec12414c46d9cedbb8fe1
Peter Xie <515971172@qq.com>
2019-01-02 23:01:50 +0800

    scene

---
+++ Assets/Scripts/MainLogic/BossShow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossShow : MonoBehaviour
+{
+    public GameObject warn;
+
+
+    private void Start()
+    {
+        DoAWarn("这个甲烷处理器，好费电，我还是关了吧");
+    }
+
+    public void DoAWarn(string str)
+    {
+        GameObject w = Instantiate(this.warn);
+        w.transform.parent = this.transform;
+        w.transform.localPosition = new Vector2(0, 70);
+        w.GetComponentInChildren<Text>().text = str;
+        //this.paramOfDoNotWarn = w;
+        this.Invoke("DoOtherWarn", 3);
+    }
+    public void DoOtherWarn()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        GameObject w = Instantiate(this.warn);
+        w.transform.parent = this.transform;
+        w.transform.localPosition = new Vector2(0, 70);
+        w.GetComponentInChildren<Text>().text = "哈哈，关掉了，这个月又省好多电";
+        //this.paramOfDoNotWarn = w;
+        this.Invoke("DoNotWarn", 20);
+    }
+    public void DoNotWarn()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Trigger/LadderWithLeaveEnable.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderWithLeaveEnable : MonoBehaviour
+{
+    public GameObject obj;
+    // 触发梯子，会使人物只能垂直移动
+    private bool enable = false;
+
+    public void Update()
+    {
+        if (enable)
+        {
+            if (GamePersist.GetInstance().hero.joy.movement.y > 0.5 || Input.GetKey(KeyCode.A))
+            {
+                GamePersist.GetInstance().hero.horzEnable = !GamePersist.GetInstance().hero.horzEnable;
+                GamePersist.GetInstance().hero.vertEnable = !GamePersist.GetInstance().hero.vertEnable;
+                enable = false;
+            }
+        }
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        this.enable = true;
+
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        this.enable = true;
+
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        this.enable = false;
+        GamePersist.GetInstance().hero.horzEnable = true;
+        GamePersist.GetInstance().hero.vertEnable = false;
+        obj.SetActive(true);
+        this.gameObject.SetActive(false);
+        
+    }
+}
