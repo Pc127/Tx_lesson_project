a79b70a13c48013e50b6165c18c46ccc5812a1ed
Peter Xie <515971172@qq.com>
2019-01-04 12:42:44 +0800

    stick

---
+++ Assets/Scripts/Trigger/StickWithEnable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickWithEnable : MonoBehaviour
+{
+    public GameObject pretrans;
+
+    private bool isStick = false;
+    private bool enable = false;
+
+    public void Update()
+    {
+        if (enable)
+        {
+            if (GamePersist.GetInstance().hero.interEnable)
+            {
+                if (!isStick)
+                {
+                    stick();
+                }
+                else
+                {
+                    unStick();
+                }
+                GamePersist.GetInstance().hero.interEnable = false;
+
+
+            }
+        }
+    }
+
+    private void stick()
+    {
+        GamePersist.GetInstance().hero.transform.parent = this.transform;
+        GamePersist.GetInstance().hero.moveEnable = false;
+        GamePersist.GetInstance().hero.DisableGravity();
+        isStick = true;
+    }
+
+    private void unStick()
+    {
+        GamePersist.GetInstance().hero.transform.parent = this.pretrans.transform;
+        GamePersist.GetInstance().hero.moveEnable = true;
+        GamePersist.GetInstance().hero.EnableGravity();
+        GamePersist.GetInstance().hero.KeepVertical();
+        isStick = false;
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        this.enable = true;
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        this.enable = false;
+        unStick();
+    }
+}
